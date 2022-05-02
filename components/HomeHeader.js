7862cfc01a472fb10bc35adc66888d90384b57a4
Harsh Kumar Raghav <72187191+HarshKumarraghav@users.noreply.github.com>
2022-05-02 16:52:12 +0530

    uploads

---
+++ components/HomeHeader.js
@@ -0,0 +1,107 @@
+import { Text, View, Image, TextInput } from "react-native";
+import React, { Component } from "react";
+import { COLORS, FONTS, SIZES, assets } from "../constants";
+export class HomeHeader extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          backgroundColor: COLORS.primary,
+          padding: SIZES.font,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={assets.logo}
+            resizeMode="contain"
+            style={{ width: 90, height: 25 }}
+          />
+          <View
+            style={{
+              width: 45,
+              height: 45,
+            }}
+          >
+            <Image
+              source={assets.person01}
+              resizeMode="contain"
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
+            <Image
+              source={assets.badge}
+              resizeMode="contain"
+              style={{
+                position: "absolute",
+                width: 15,
+                height: 15,
+                bottom: 0,
+                right: 0,
+              }}
+            />
+          </View>
+        </View>
+        <View style={{ marginVertical: SIZES.font }}>
+          <Text
+            style={{
+              fontFamily: FONTS.regular,
+              fontSize: SIZES.small,
+              color: COLORS.white,
+            }}
+          >
+            Hello, Raghav 👋
+          </Text>
+          <Text
+            style={{
+              fontFamily: FONTS.bold,
+              fontSize: SIZES.large,
+              color: COLORS.white,
+              marginTop: SIZES.base / 2,
+            }}
+          >
+            Let's find a Masterpeice
+          </Text>
+        </View>
+        <View style={{ marginTop: SIZES.font }}>
+          <View
+            style={{
+              width: "100%",
+              borderRadius: SIZES.font,
+              backgroundColor: COLORS.gray,
+              flexDirection: "row",
+              alignItems: "center",
+              paddingHorizontal: SIZES.font,
+              paddingVertical: SIZES.small -2
+            }}
+
+          >
+            <Image
+              source={assets.search}
+              resizeMode="contain"
+              style={{
+                width: 20,
+                height: 20,
+                marginRight: SIZES.base,
+              }}
+            />
+            <TextInput
+              placeholder="Search NFT"
+              style={{ flex: 1 }}
+              onChangeText={() => {}}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default HomeHeader;
---
+++ components/index.js
@@ -0,0 +1,20 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import DetailsBid from "./DetailsBid";
+import DetailDesc from "./DetailDesc";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, NFTTitle, EthPrice, EndDate } from "./SubInfo";
+export {
+  CircleButton,
+  RectButton,
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  DetailsBid,
+  DetailDesc,
+  SubInfo,
+  NFTTitle,
+  EthPrice,
+  EndDate,
+};
